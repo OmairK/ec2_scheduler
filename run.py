4e4aa7c625128c6313dae86d5967b36427727daa
Omair Khan <omairkhan064@gmail.com>
2021-03-31 15:59:41 +0530

    Flask skeleton

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import data
+
+app = Flask(__name__)
+
+# Import the API routes
+from routes.endpoints import *
+
+# Required because app is imported in other modules
+if __name__== '__main__':
+    app.run(debug=True)
