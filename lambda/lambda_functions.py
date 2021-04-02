5cfd019d18cd1c40aca3806c01811be078b353a5
Omair Khan <omairkhan064@gmail.com>
2021-04-02 05:34:55 +0530

    Updated lambda function

--- lambda/lambda_functions.py
+++ lambda/lambda_functions.py
@@ -1,7 +1,4 @@
-import json
-
 import boto3
-print('Loading function')
 
 def lambda_handler(event, context):
 	print('------------------------')
@@ -10,9 +7,9 @@ def lambda_handler(event, context):
 	try:
 		for record in event['Records']:
 			#2. Handle event by type
-			if record['dynamodb']['state'] == 'up':
+			if record['dynamodb']['state'] == 'started':
 				start_instance(record)
-			elif record['dynamodb']['state'] == 'down':
+			elif record['dynamodb']['state'] == 'stopped':
 				stop_instance(record)
 		print('------------------------')
 		return "Success!"
@@ -27,6 +24,9 @@ def start_instance(record):
     ec2_resource = boto3.resource("ec2")
     #TODO: Error handling in case of wrong ec2 instance
     ec2_instance = ec2_resource.Instance(ec2_id)
+    if record["allowScheduling"] == "true":
+        try:
+            ec2_instance.create_tags(Tags=[{"Key": "scheduled","Value": "true"}])
     ec2_instance.start()
 	print(f'EC2 instance with {ec2_id} started')
 
@@ -36,5 +36,8 @@ def handle_modify(record):
     ec2_resource = boto3.resource("ec2")
     #TODO: Error handling in case of wrong ec2 instance
     ec2_instance = ec2_resource.Instance(ec2_id)
+    if record["allowScheduling"] == "false":
+        try:
+            ec2_instance.delete_tags(Tags=[{"Key": "scheduled","Value": "true"}])
     ec2_instance.stop()
 	print(f'EC2 instance with {ec2_id} stopped')
